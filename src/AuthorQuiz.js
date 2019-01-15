2fe47517fd52139d4efe847e8b33e23d465fe894
renata_jachtoma_mendak <prana861@gmail.com>
2019-01-15 16:37:21 +0100

    add file

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,28 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+function Hero(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        <div className='row'>
+            <div className="jumbotron col-10 offset-1">
+                <h1>Author quiz</h1>
+                <p>Wybierz poprawną odpowiedź na zadane pytania</p>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+function Turn({author,books, hightlight}){
+    function heilightColour (hightlight){
+        const mapping= {
+            'none':'',
+            'correct':'green',
+            'wrong':'red'
+        }
+        return mapping[hightlight];
+    }
+    return (
+        <div className='row turn' style={{backgroundColor:heilightColour(hightlight)}}>
+            <div className='col-4 offset-1'>
+                <img src={author.imgUrl} alt="Author" className='authorimage'/>
+            </div>
+            <div className='col-4'>
+                {books.map((title) => <Book title={title} key={title}/>)}
+            </div>
+        </div>
+    )
+}
+
+function Book ({title}){
+    return (
+        <div className='answer'>
+            <h3>{title}</h3>
+        </div>
+    )
+}
+function Continue(){
+    return (
+        <div className='row'>
+
+        </div>
+    )
+}
+
+function Footer(){
+    return (
+        <div className='row' id='footer'>
+            <div className='col-12'>
+                <p className='text-muted credit'>
+                    wszystkie obrazki sa ze strony <a href='https://commons.wikimedia.org/wiki/Main_Page'>z linka</a>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+function AuthorQuiz({turnData, hightlight}) {
+
+    return (
+      <div className='container-fluid'>
+        <Hero/>
+        <Turn {...turnData} hightlight={hightlight} />
+        <Continue/>
+        <Footer/>
+      </div>
+    );
+}
+
+export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const authors =[
+    {
+        books:"CouchDB: The Definitive Guide",
+        name: "Mark Twain",
+        imgUrl:'images/mark.jpg'
+    },{
+        books: "Anna Karenina",
+        name: "Leo Tolstoy",
+        imgUrl: "images/lew.jpg",
+    },
+    {
+        books: "Pan Tadeusz",
+        name: "Adam Mickiewicz",
+        imgUrl: "images/tadeusz.jpg",
+    },
+    {
+        books: "The Adventures of Huckleberry Finn",
+        name: "Mark Twain",
+        imgUrl: "images/mark.jpg",
+    },
+
+]
+
+
+function getTurnData(authors){
+    const allbooks= authors.reduce(function(p,c) {
+        return p.concat(c.books);
+    },[]);
+    const fourRandomBooks = shuffle(allbooks).slice(0,4);
+    const answer = sample (fourRandomBooks)
+    return{
+        books:fourRandomBooks,
+        author:  authors.find((author) => author.books)
+        // author: authors.find((author) => author.books.some((title) => title ===answer))
+    }
+
+}
+
+const state = {
+    turnData : getTurnData(authors),
+    hightlight: 'correct'
+
+};
+
+ReactDOM.render(<AuthorQuiz {...state}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
